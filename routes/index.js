9df4fde2a17bd58391ba8cbbd60468f3dc19dd2e
Blaise Fringel <ffringel9@gmail.com>
2019-05-04 22:24:04 -0500

    create data folder and import json data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
+var data = require("./../data/item-data.json");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.json(data);
 });
 
 module.exports = router;
